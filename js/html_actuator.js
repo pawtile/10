66c439da0bb1f84111e0f202c98a2ee3d28b95bd
pawtile <x0t1xxreal@gmail.com>
2025-10-28 21:22:42 +0000

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -82,8 +82,9 @@ HTMLActuator.prototype.addTile = function (tile) {
   if (tile.value === -19) inner.textContent = 19;
   if (tile.value === -20) inner.textContent = 20;
   if (tile.value === -21) inner.textContent = 21;
+  if (tile.value === -34) inner.textContent = 34;
   if (tile.value === -45) inner.textContent = 45;
-  if (tile.value === -49) inner.textContent = 49;
+  if (tile.value === -48) inner.textContent = 48;
   if (tile.value === -50) inner.textContent = 50;
   if (tile.value === -57) inner.textContent = 57;
   if (tile.value === -61) inner.textContent = 61;
